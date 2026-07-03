62b5cc5a4e8d9037e4aa76c72b0f0f343616e261
apachertr180navneeet <navneet@brenin.co>
2026-07-03 12:38:54 +0530

    feat: implement premium glassmorphism admin theme with global layout styling

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -12,7 +12,7 @@
         <link rel="icon" type="image/x-icon" href="{{asset('assets/admin/img/favicon/favicon.ico')}}" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="{{asset('assets/admin/vendor/fonts/boxicons.css')}}" />
         <link rel="stylesheet" href="{{asset('assets/admin/vendor/css/core.css')}}" class="template-customizer-core-css" />
         <link rel="stylesheet" href="{{asset('assets/admin/vendor/css/theme-default.css')}}" class="template-customizer-theme-css" />
@@ -24,6 +24,7 @@
         <script src="{{asset('assets/admin/js/config.js')}}"></script>
         <link rel="stylesheet" href="{{asset('assets/admin/css/sweet-alert.css')}}" />
         <link rel="stylesheet" href="{{asset('assets/admin/css/custom.css')}}" />
+        <link rel="stylesheet" href="{{asset('assets/admin/css/premium.css')}}" />
         @yield('style')
         <style>
             
